4cc801228cac5f8e79fc7fa5454b4787d6ec5f52
Zdeněk Doležal <Griperis@outlook.cz>
2019-10-19 18:58:44 +0200

    test

--- plgnd/plgnd/settings.py
+++ plgnd/plgnd/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenosie.'
 ]
 
 ROOT_URLCONF = 'plgnd.urls'
