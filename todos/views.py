1a47b83d62e0ae11eb7bc76d05bea7efe28e7636
Zdeněk Doležal <Griperis@outlook.cz>
2019-10-20 13:56:22 +0200

    working + and - off todos

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.template import loader
 from .models import Todo
-# Create your views here.
+
 def index(request):
+    if request.POST:
+        new_title = request.POST.get('title')
+        new_text = request.POST.get('text-detail')
+        new_todo = Todo(title_text=new_title, detail=new_text)
+        new_todo.save()
     todos = Todo.objects.all()
     return render(request, 'todos/index.html', {'todos': todos})
+
+def delete_todo(request, id):
+    todo = get_object_or_404(Todo, pk=id)
+    todo.delete()
+    return redirect('../')
